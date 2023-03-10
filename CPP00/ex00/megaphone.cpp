eb48059f16f4d09fd3cadf8e408b7417da8fea04
Ghost1215 <solugeniawollem@gmail.com>
2023-03-10 13:56:34 +0300

    added

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cout;
+
+
+int main(int ac, char **av)
+{
+    int j = 1;
+    int i = 0;
+
+    if (ac > 1)
+    {
+        while (av[j]) {
+            i = 0;
+            while (av[j][i])
+            {
+                cout << (char)toupper(av[j][i]);
+                i++;
+            }
+            j++;
+        }
+        cout << "\n";
+    }
+    else
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+    return (0);
+}
