df2880b7468f9d5d9295586bc46f841a08f8ede0
Ghost1215 <solugeniawollem@gmail.com>
2023-03-11 17:37:48 +0300

    added

--- CPP00/ex01/Phonebook.cpp
+++ CPP00/ex01/Phonebook.cpp
@@ -1,26 +1,63 @@
-#include "Phonebook.hpp"
+#include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook() : index(0) {
-    return;
+#include <iostream>
+#include <iomanip>
+
+PhoneBook::PhoneBook() : numContacts(0) {}
+
+PhoneBook::~PhoneBook() {}
+
+bool PhoneBook::addContact(const Contact &contact)
+{
+    if (numContacts < max_capacity)
+    {
+        contacts[numContacts] = contact;
+        numContacts++;
+        return true;
+    }
+    else
+    {
+        for (int i = 0; i < max_capacity - 1; i++)
+        {
+            contacts[i] = contacts[i + 1];
+        }
+        contacts[max_capacity - 1] = contact;
+        return true;
+    }
+    return false;
 }
 
-PhoneBook::~PhoneBook() {
-    return;
+void PhoneBook::searchContacts() const
+{
+    std::cout << std::setw(10) << "Index"
+              << "|"
+              << std::setw(20) << "First Name"
+              << "|"
+              << std::setw(20) << "Last Name"
+              << "|"
+              << std::setw(20) << "Phone Number"
+              << "|" << std::endl;
+    std::cout << std::setfill('-') << std::setw(73) << "-" << std::endl;
+    std::cout << std::setfill(' ');
+
+    for (int i = 0; i < numContacts; i++)
+    {
+        std::cout << std::setw(10) << i << "|"
+                  << std::setw(20) << contacts[i].getFirstName() << "|"
+                  << std::setw(20) << contacts[i].getLastName() << "|"
+                  << std::setw(20) << contacts[i].getPhoneNumber() << "|" << std::endl;
+    }
+
+    if (numContacts == 0)
+    {
+        std::cout << "The phonebook is empty." << std::endl;
+    }
+}
+
+int PhoneBook::getSize() const {
+    return numContacts;
 }
 
 Contact PhoneBook::getContact(int index) const {
     return contacts[index];
 }
-
-void PhoneBook::setContact( Contact _contacts ) {
-    contacts[index] = _contacts;
-    count = (count < 8) ? count + 1 : count;
-    index = (index + 1) % 8;
-    return;
-}
-
-int PhoneBook::getCount() const {
-    return count;
-}
-
-int PhoneBook::count = 0;
