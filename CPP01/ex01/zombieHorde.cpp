6f9b171d330ca6847cbfa9559e355a0a36c4bc46
Ghost1215 <solugeniawollem@gmail.com>
2023-03-13 02:44:48 +0300

    added

---
+++ CPP01/ex01/zombieHorde.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie *zombieHorde(int N, std::string name)
+{
+    Zombie *horde = new Zombie[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        horde[i] = Zombie(name);
+    }
+
+    return horde;
+}
