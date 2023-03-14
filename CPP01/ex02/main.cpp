f79cc55ffe83ad1a870ab237375670ee403aa4c3
Ghost1215 <solugeniawollem@gmail.com>
2023-03-15 01:57:17 +0300

    added

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+
+    std::cout << "String'in bellek adresi: " << &str << std::endl;
+    std::cout << "stringPTR'nin tuttugu bellek adresi: " << stringPTR << std::endl;
+    std::cout << "stringREF'nin tuttugu bellek adresi: " << &stringREF << std::endl;
+
+
+    std::cout << "String degişkeninin degeri: " << str << std::endl;
+    std::cout << "stringPTR tarafindan isaret edilen deger: " << *stringPTR << std::endl;
+    std::cout << "stringREF tarafindan isaret edilen deger: " << stringREF << std::endl;
+
+    return (0);
+}
