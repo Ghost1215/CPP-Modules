47621caa649e0a0de8e04e68babca474299da025
Ghost1215 <solugeniawollem@gmail.com>
2023-03-18 17:46:08 +0300

    updated

--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -5,17 +5,17 @@ Weapon::Weapon()
     this->type = "";
 }
 
-Weapon::Weapon(const std::string &type)
+Weapon::Weapon(const string &type)
 {
     setType(type);
 }
 
-const std::string &Weapon::getType() const
+const string &Weapon::getType() const
 {
     return type;
 }
 
-void Weapon::setType(const std::string &type)
+void Weapon::setType(const string &type)
 {
     this->type = type;
 }
