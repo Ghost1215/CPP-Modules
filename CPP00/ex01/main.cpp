aaaee00a4e822b95a53907334b1dda85d4a1b03f
Ghost1215 <solugeniawollem@gmail.com>
2023-03-18 15:22:51 +0300

    updated

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,47 +1,45 @@
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
 
-#include <iostream>
-
 int main()
 {
     PhoneBook phonebook;
-    std::string command;
+    string command;
 
-    std::cout << "Welcome to the 80's! This is your Phone Book.\n"
+    cout << "Welcome to the 80's! This is your Phone Book.\n"
               << "You can ADD, SEARCH or EXIT. What would you like to do?\n";
 
-    while (std::getline(std::cin, command))
+    while (getline(cin, command))
     {
         if (command == "ADD")
         {
-            std::string firstName, lastName, nickname, phoneNumber, darkSecret;
-            std::cout << "Enter first name: ";
-            std::getline(std::cin, firstName);
-            std::cout << "Enter last name: ";
-            std::getline(std::cin, lastName);
-            std::cout << "Enter nickname: ";
-            std::getline(std::cin, nickname);
-            std::cout << "Enter phone number: ";
-            std::getline(std::cin, phoneNumber);
-            std::cout << "Enter darkest secret: ";
-            std::getline(std::cin, darkSecret);
+            string firstName, lastName, nickname, phoneNumber, darkSecret;
+            cout << "Enter first name: ";
+            getline(cin, firstName);
+            cout << "Enter last name: ";
+            getline(cin, lastName);
+            cout << "Enter nickname: ";
+            getline(cin, nickname);
+            cout << "Enter phone number: ";
+            getline(cin, phoneNumber);
+            cout << "Enter darkest secret: ";
+            getline(cin, darkSecret);
 
             Contact contact(firstName, lastName, nickname, phoneNumber, darkSecret);
             phonebook.addContact(contact);
-            std::cout << "Contact added to phone book.\n";
+            cout << "Contact added to phone book.\n";
         }
         else if (command == "SEARCH")
         {
             phonebook.searchContacts();
-            std::cout << "Enter index of contact to view: ";
+            cout << "Enter index of contact to view: ";
 
             int index;
-            std::cin >> index;
+            cin >> index;
 
             if (index < 0 || index > phonebook.getSize())
             {
-                std::cout << "Invalid index.\n";
+                cout << "Invalid index.\n";
             }
             else
             {
@@ -50,12 +48,12 @@ int main()
         }
         else if (command == "EXIT")
         {
-            std::cout << "Goodbye!\n";
+            cout << "Goodbye!\n";
             return (0);
         }
         else
         {
-            std::cout << "Invalid command. Please try again.\n";
+            cout << "Invalid command. Please try again.\n";
         }
     }
 
