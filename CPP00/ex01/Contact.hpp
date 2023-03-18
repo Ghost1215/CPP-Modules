fc8141469571c65a94bea573c950d297ef23ea89
Ghost1215 <solugeniawollem@gmail.com>
2023-03-18 15:22:19 +0300

    updated

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -3,36 +3,44 @@
 
 #include <string>
 #include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::setw;
+using std::setfill;
 
 class Contact
 {
 private:
-    std::string firstName;
-    std::string lastName;
-    std::string nickname;
-    std::string phoneNumber;
-    std::string darkSecret;
+    string firstName;
+    string lastName;
+    string nickname;
+    string phoneNumber;
+    string darkSecret;
 
 public:
     Contact();
 
-    Contact(const std::string &firstName, const std::string &lastName,
-            const std::string &nickname, const std::string &phoneNumber,
-            const std::string &darkSecret);
+    Contact(const string &firstName, const string &lastName,
+            const string &nickname, const string &phoneNumber,
+            const string &darkSecret);
 
     ~Contact();
 
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getNickname() const;
-    std::string getPhoneNumber() const;
-    std::string getDarkSecret() const;
+    string getFirstName() const;
+    string getLastName() const;
+    string getNickname() const;
+    string getPhoneNumber() const;
+    string getDarkSecret() const;
 
-    void setFirstName(const std::string &firstName);
-    void setLastName(const std::string &lastName);
-    void setNickname(const std::string &nickname);
-    void setPhoneNumber(const std::string &phoneNumber);
-    void setDarkSecret(const std::string &darkSecret);
+    void setFirstName(const string &firstName);
+    void setLastName(const string &lastName);
+    void setNickname(const string &nickname);
+    void setPhoneNumber(const string &phoneNumber);
+    void setDarkSecret(const string &darkSecret);
 
     void printFullInfo() const;
 };
