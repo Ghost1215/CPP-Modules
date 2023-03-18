b1b2cd8a1b4ace66021d89eea7909327acc7da9b
Ghost1215 <solugeniawollem@gmail.com>
2023-03-18 23:19:58 +0300

    updated

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -1,8 +1,8 @@
 #include "Harl.hpp"
 
-void Harl::complain(std::string level)
+void Harl::complain(string level)
 {
-    std::string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+    string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
 
     void (Harl::*comments[4])();
 
@@ -20,20 +20,20 @@ void Harl::complain(std::string level)
 
 void Harl::debug()
 {
-    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+    cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << endl;
 }
 
 void Harl::info()
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << endl;
 }
 
 void Harl::warning()
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << endl;
 }
 
 void Harl::error()
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    cout << "This is unacceptable! I want to speak to the manager now." << endl;
 }
