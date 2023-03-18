03baa4e2c045ecf0a54d0adb3fe510408bf415cb
Ghost1215 <solugeniawollem@gmail.com>
2023-03-18 17:46:31 +0300

    updated

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -1,8 +1,8 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string &name) : name(name), weapon(NULL) {}
+HumanB::HumanB(const string &name) : name(name), weapon(NULL) {}
 
-HumanB::HumanB(const std::string &name, Weapon *weapon) : name(name), weapon(weapon) {}
+HumanB::HumanB(const string &name, Weapon *weapon) : name(name), weapon(weapon) {}
 
 HumanB::~HumanB() {}
 
@@ -19,10 +19,10 @@ void HumanB::attack() const
 {
     if (weapon != NULL)
     {
-        std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+        cout << name << " attacks with their " << weapon->getType() << endl;
     }
     else
     {
-        std::cout << name << " hands up!" << std::endl;
+        cout << name << " hands up!" << endl;
     }
 }
