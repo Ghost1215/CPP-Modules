72073bcbc6e8cf92de9c76743daad62560af3ddb
Ghost1215 <solugeniawollem@gmail.com>
2023-03-30 21:29:12 +0300

    added

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,33 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+
+class Fixed
+{
+private:
+    int fixedValue;
+    static const int fractionalBits = 8;
+
+public:
+    Fixed(void);
+    Fixed(const int val);
+    Fixed(const float val);
+    Fixed(const Fixed &other);
+    ~Fixed(void);
+
+    Fixed &operator=(const Fixed &other);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+    float toFloat(void) const;
+    int toInt(void) const;
+};
+
+ostream &operator<<(ostream &os, const Fixed &fixed);
+
+#endif
