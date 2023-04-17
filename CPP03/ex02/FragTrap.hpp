7b6f94a5adb0aeeda7b683f465b6923be8d804be
Ghost1215 <solugeniawollem@gmail.com>
2023-04-17 16:39:33 +0300

    added

---
+++ CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+    FragTrap();
+    FragTrap(const string name);
+    FragTrap(const FragTrap &other);
+    ~FragTrap();
+
+    FragTrap &operator=(const FragTrap &other);
+
+    void highFivesGuys(void);
+};
+
+#endif
