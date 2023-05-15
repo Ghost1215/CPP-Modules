42001327d195bae89dd85a3066458cc540e1e3d9
Ghost1215 <solugeniawollem@gmail.com>
2023-05-16 02:47:22 +0300

    added

---
+++ CPP04/ex02/Brain.cpp
@@ -0,0 +1,29 @@
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+	cout << "Brain default constructor called." << endl;
+}
+
+Brain::Brain(const Brain &oth)
+{
+	*this = oth;
+	cout << "Brain copy constructor called." << endl;
+}
+
+Brain::~Brain()
+{
+	cout << "Brain destroyed." << endl;
+}
+
+Brain &Brain::operator=(const Brain &oth)
+{
+	int	i = -1;
+	
+	while (++i < 100)
+		this->ideas[i] = oth.ideas[i];
+
+	cout << "Brain copy assigment operator called." << endl;
+
+	return (*this);
+}
