3c1e4d48c3d5a0655517c04d34cd1aa3709f09d3
Ghost1215 <solugeniawollem@gmail.com>
2023-05-16 02:47:13 +0300

    added

---
+++ CPP04/ex02/Animal.cpp
@@ -0,0 +1,35 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+	type = "Animal";
+	cout << "Animal default constructor called." << endl;
+}
+
+Animal::Animal(const Animal &oth)
+{
+	*this = oth;
+	cout << "Animal copy constructor called." << endl;
+}
+
+Animal::~Animal()
+{
+	cout << "Animal destroyed." << endl;
+}
+
+Animal	&Animal::operator=(const Animal &oth)
+{
+	type = oth.type;
+	cout << "Animal copy assigment operator called." << endl;
+	
+	return *this;
+}
+
+void Animal::makeSound() const
+{
+	cout << "The Animal Sound" << endl;
+}
+
+string	Animal::getType() const { 
+	return type; 
+}
---
+++ CPP04/ex02/Animal.hpp
@@ -0,0 +1,26 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Animal
+{
+protected:
+	string type;
+	
+public:
+	Animal();
+	Animal(const Animal &oth);
+	virtual ~Animal();
+	
+	Animal &operator=(const Animal& oth);
+	
+	virtual void makeSound() const = 0;
+	string getType() const;
+};
+
+#endif
