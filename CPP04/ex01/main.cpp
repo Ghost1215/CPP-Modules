e0acda6830d73c043a81748371e6b71a016992b3
Ghost1215 <solugeniawollem@gmail.com>
2023-05-17 17:38:01 +0300

    updated

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -3,27 +3,60 @@
 #include "Dog.hpp"
 #include "WrongCat.hpp"
 
+// int main()
+// {
+// 	Animal *animals[6];
+
+// 	for (int i = 0; i < 3; ++i)
+// 	{
+// 		cout << "dog: " << i << endl;
+// 		animals[i] = new Dog();
+// 	}
+
+// 	for (int i = 3; i < 6; ++i)
+// 	{
+// 		cout << "cat: " << i << endl;
+// 		animals[i] = new Cat();
+// 	}
+
+// 	for (int i = 0; i < 6; ++i)
+// 	{
+// 		cout << "destruct: " << i << endl;
+// 		delete animals[i];
+// 	}
+
+// 	return (0);
+// }
+
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include <iomanip>
+
 int main()
 {
-	Animal *animals[6];
+	Animal *animals[10];
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < 10; i++)
 	{
-		cout << "dog: " << i << endl;
-		animals[i] = new Dog();
+		if (i >= 5)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
 	}
 
-	for (int i = 3; i < 6; ++i)
-	{
-		cout << "cat: " << i << endl;
-		animals[i] = new Cat();
-	}
+	Cat copy(*(Cat *)animals[0]);
 
-	for (int i = 0; i < 6; ++i)
+	cout << endl;
+	
+	for (int i = 0; i < 100; i++)
 	{
-		cout << "destruct: " << i << endl;
+		cout << setw(30) << left << copy.getBrain()->getIdea(i);
+		cout << setw(30) << left << ((Cat *)animals[0])->getBrain()->getIdea(i) << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 10; i++)
+	{
 		delete animals[i];
 	}
-
-	return (0);
+	// system("leaks Animals");
 }
