fd73d266f56c2102e64aa39750901eb5c3052262
Ghost1215 <solugeniawollem@gmail.com>
2023-05-18 21:54:41 +0300

    updated

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -7,7 +7,7 @@ class PhoneBook
 {
 private:
     int numContacts;
-    
+
     static const int max_capacity = 8;
     Contact contacts[max_capacity];
 
@@ -17,6 +17,8 @@ public:
 
     bool addContact(const Contact &contact);
     void searchContacts() const;
+    void contactControl(string &firstName, string &lastName, string &nickname,
+                        string &phoneNumber, string &darkSecret);
     int getSize() const;
     Contact getContact(int index) const;
 };
