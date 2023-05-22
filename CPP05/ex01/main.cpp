1d409d48a556d55778081f7cd318dae9b9fc66ed
Ghost1215 <solugeniawollem@gmail.com>
2023-05-22 04:03:34 +0300

    added

---
+++ CPP05/ex01/main.cpp
@@ -0,0 +1,26 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main() 
+{
+    try {
+        Bureaucrat bureaucrat("John Doe", 50);
+        cout << bureaucrat << endl;
+
+        Form form("Tax Form", 75, 100);
+        cout << form << endl;
+
+        bureaucrat.signForm(form);
+        cout << form << endl;
+
+        bureaucrat.incrementGrade();
+        cout << bureaucrat << endl;
+
+        bureaucrat.signForm(form);
+        cout << form << endl;
+    } catch (const exception& e) {
+        cout << "Exception: " << e.what() << endl;
+    }
+
+    return 0;
+}
