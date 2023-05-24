af89304375b223188b0d47dffa9e9b4f4a6fc19b
Ghost1215 <solugeniawollem@gmail.com>
2023-05-25 01:33:36 +0300

    added

---
+++ CPP05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,21 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(const string &target)
+    : AForm("Robotomy Request Form", 72, 45), target(target) {}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+void RobotomyRequestForm::execute(const Bureaucrat &executor) const
+{
+    if (!isSigned())
+        throw AForm::GradeTooLowException();
+
+    if (executor.getGrade() > getExecuteGrade())
+        throw AForm::GradeTooLowException();
+
+    cout << "Drilling noises..." << endl;
+    if (rand() % 2 == 0)
+        cout << target << " has been robotomized successfully." << endl;
+    else
+        cout << "Robotomy failed for " << target << "." << endl;
+}
