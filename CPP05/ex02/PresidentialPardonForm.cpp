6ca41ebcaf7a283c4bed2f8880db9f9d80b9bade
Ghost1215 <solugeniawollem@gmail.com>
2023-05-25 01:33:17 +0300

    added

---
+++ CPP05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,17 @@
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(const string &target)
+    : AForm("Presidential Pardon Form", 25, 5), target(target) {}
+
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+void PresidentialPardonForm::execute(const Bureaucrat &executor) const
+{
+    if (!isSigned())
+        throw AForm::GradeTooLowException();
+
+    if (executor.getGrade() > getExecuteGrade())
+        throw AForm::GradeTooLowException();
+
+    cout << target << " has been pardoned by Zaphod Beeblebrox." << endl;
+}
