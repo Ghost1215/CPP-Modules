0d2c17b4ef3cae618284603b831e8b95e6d1d32a
Ghost1215 <solugeniawollem@gmail.com>
2023-05-25 01:33:10 +0300

    added

---
+++ CPP05/ex02/Bureaucrat.cpp
@@ -0,0 +1,81 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const string &name, int grade)
+    : name(name), grade(grade)
+{
+    if (grade < 1)
+        throw GradeTooHighException();
+    else if (grade > 150)
+        throw GradeTooLowException();
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+const string &Bureaucrat::getName() const
+{
+    return (name);
+}
+
+int Bureaucrat::getGrade() const
+{
+    return (grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+    if (grade == 1)
+        throw GradeTooHighException();
+    else
+        grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if (grade == 150)
+        throw GradeTooLowException();
+    else
+        grade++;
+}
+
+void Bureaucrat::signForm(AForm &form) const
+{
+    try
+    {
+        form.beSigned(*this);
+        cout << *this << " signs " << form << endl;
+    }
+    catch (const exception &e)
+    {
+        cout << *this << " cannot sign " << form << " because " << e.what() << endl;
+    }
+}
+
+void Bureaucrat::executeForm(const AForm &form) const
+{
+    try
+    {
+        form.execute(*this);
+        cout << *this << " executed " << form << endl;
+    }
+    catch (const exception &e)
+    {
+        cout << *this << " cannot execute " << form << " because " << e.what() << endl;
+    }
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Bureaucrat grade is too high!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Bureaucrat grade is too low!";
+}
+
+ostream &operator<<(ostream &os, const Bureaucrat &bureaucrat)
+{
+    os << "Bureaucrat: " << bureaucrat.getName() << ", Grade: " << bureaucrat.getGrade();
+
+    return (os);
+}
