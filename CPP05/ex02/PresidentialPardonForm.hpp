182249f6fdd1343064345c3196907ec369b0950e
Ghost1215 <solugeniawollem@gmail.com>
2023-05-25 01:32:35 +0300

    added

---
+++ CPP05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,18 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+private:
+    const string target;
+
+public:
+    PresidentialPardonForm(const string &target);
+    ~PresidentialPardonForm();
+
+    void execute(const Bureaucrat &executor) const;
+};
+
+#endif
