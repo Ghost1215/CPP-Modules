8c23ca60a4b26431e51c6c4c26aaa0563b57866e
Ghost1215 <solugeniawollem@gmail.com>
2023-05-25 01:32:28 +0300

    added

---
+++ CPP05/ex02/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "AForm.hpp"
+
+using std::cout;
+using std::endl;
+using std::exception;
+using std::ostream;
+using std::string;
+
+class AForm;
+
+class Bureaucrat
+{
+private:
+    const string name;
+    int grade;
+
+public:
+    Bureaucrat(const string &name, int grade);
+    ~Bureaucrat();
+
+    const string &getName() const;
+    int getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
+    void signForm(AForm &form) const;
+    void executeForm(const AForm &form) const;
+
+    class GradeTooHighException : public exception
+    {
+    public:
+        const char *what() const throw();
+    };
+
+    class GradeTooLowException : public exception
+    {
+    public:
+        const char *what() const throw();
+    };
+};
+
+ostream &operator<<(ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
