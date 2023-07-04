6c341a6b6d62a36d5c81f5d75e0deaa12ba6c268
Ghost1215 <solugeniawollem@gmail.com>
2023-07-05 02:58:29 +0300

    added

---
+++ CPP05/ex03/Intern.cpp
@@ -0,0 +1,41 @@
+#include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+
+Intern::Intern() {}
+
+Intern::~Intern() {}
+
+AForm *Intern::makeForm(const string &AFormName, const string &target) const
+{
+    string AFormNames[3] = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"};
+
+    int i = 0;
+    while (i < 3 && AFormNames[i] != AFormName)
+        i++;
+
+    AForm *tmp = NULL;
+
+    switch (i)
+    {
+    case 0:
+        tmp = new ShrubberyCreationForm(target);
+        break;
+    case 1:
+        tmp = new RobotomyRequestForm(target);
+        break;
+    case 2:
+        tmp = new PresidentialPardonForm(target);
+        break;
+    default:
+        throw Intern::InvalidAFormException();
+    }
+
+    cout << "Intern creates " << AFormName << endl;
+
+    return (tmp);
+}
