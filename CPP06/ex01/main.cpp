4aae0184039d6efa8112d391d26d8f952bdce00b
Ghost1215 <solugeniawollem@gmail.com>
2023-12-28 21:59:14 +0300

    main added

---
+++ CPP06/ex01/main.cpp
@@ -0,0 +1,21 @@
+#include "Data.hpp"
+
+int main() {
+    t_data* originalData = new t_data{"btekinli", 24, 7.14};
+
+    uintptr_t serializedPtr = serialize(originalData);
+
+    t_data* deserializedData = deserialize(serializedPtr);
+
+    if (originalData == deserializedData) {
+        std::cout << "Serialization and deserialization successful." << std::endl;
+        std::cout << "Username: " << deserializedData->username << "\nAge: " << deserializedData->age
+                  << "\nLevel: " << deserializedData->level << std::endl;
+    } else {
+        std::cout << "Serialization and deserialization failed." << std::endl;
+    }
+
+    delete originalData;
+
+    return 0;
+}
