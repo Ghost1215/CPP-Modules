2e6f1e76a26d826aa9e488b5618c2fc953ae1cb4
Ghost1215 <solugeniawollem@gmail.com>
2024-03-05 02:03:07 +0300

    updated

--- CPP05/ex02/PresidentialPardonForm.cpp
+++ CPP05/ex02/PresidentialPardonForm.cpp
@@ -28,7 +28,7 @@ void PresidentialPardonForm::execute(Bureaucrat const &executor) const
 
 	if (executor.getGrade() > getGradeToExecute())
 		throw AForm::GradeTooLowException();
-	cout << target << " has been pardoned by Zafod Beeblebrox." << endl;
+	cout << target << " has been pardoned by Zaphod Beeblebrox." << endl;
 }
 
 PresidentialPardonForm::~PresidentialPardonForm() {}
