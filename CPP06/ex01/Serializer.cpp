8510bb04d3a31a2a5987409d3de58ba8046063bc
Ghost1215 <solugeniawollem@gmail.com>
2024-03-06 02:11:16 +0000

    updated

--- CPP06/ex01/Serializer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Serializer.hpp"
-
-std::uintptr_t Serializer::serialize(Data *data){
-	return reinterpret_cast<uintptr_t>(data);
-}
-
-Data* Serializer::deserialize(uintptr_t ptr){
-	return reinterpret_cast<Data *>(ptr);
-}
