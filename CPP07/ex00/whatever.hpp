b38a5ec04dee85416df59818a32f9907d119191b
Ghost1215 <solugeniawollem@gmail.com>
2024-03-07 11:56:33 +0000

    updated

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -4,19 +4,22 @@
 #include <iostream>
 
 template <typename T>
-void swap(T& a, T& b) {
+void swap(T &a, T &b)
+{
     T temp = a;
     a = b;
     b = temp;
 }
 
 template <typename T>
-T min(T& a, T& b) {
+T min(T &a, T &b)
+{
     return a < b ? a : b;
 }
 
 template <typename T>
-T max(T& a, T& b) {
+T max(T &a, T &b)
+{
     return a > b ? a : b;
 }
 
